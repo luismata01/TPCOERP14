6aa874ad2278c0f08966bb42a279e2d6d6be5441
wmoreno <walter.moreno@tpco.cl>
2021-12-28 11:39:24 -0300

    Couties y documentonline

---
+++ l10n_cl_counties/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Chile Localization Regions, Cities and Counties",
+    "version": "1.2.0",
+    "author": "Blanco Martín & Asociados",
+    "license": "LGPL-3",
+    "website": "http://blancomartin.cl",
+    "category": "Localization/Geopolitical Distribution",
+    "depends": [
+        "l10n_cl",
+        "base_address_city",
+    ],
+    "data": [
+        "data/res.country.state.csv",
+        "data/res.city.csv",
+        "views/res_partner_view.xml",
+        "views/res_state_view.xml",
+        "views/res_company_view.xml",
+    ],
+    "active": False,
+    "installable": True,
+}
---
+++ l10n_cl_counties/models/res_partner.py
@@ -0,0 +1,48 @@
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    def _default_country(self):
+        try:
+            return self.env.user.company_id.country_id or False
+        except:
+            return False
+
+    country_id = fields.Many2one("res.country", default=_default_country)
+    state_id = fields.Many2one("res.country.state", compute='_change_state_province', store=True, readonly=False,
+        string='Ubication', domain="[('country_id', '=', country_id), ('type', '=', 'normal')]")
+    real_city = fields.Char(compute='_change_real_city_province', string='City.')
+    city = fields.Char(compute='_change_city_province', string='City', store=True, readonly=False)
+    country_code = fields.Char(related='country_id.code', string='Country ID')
+    company_country_code = fields.Char(compute='_get_company_country_code', string='Country ID')
+
+    @api.depends('country_id', 'company_id')
+    def _get_company_country_code(self):
+        for record in self:
+            if record.company_id.country_id.code == 'CL' or self.env.company.country_id.code == 'CL':
+                record.company_country_code = 'CL'
+            else:
+                record.company_country_code = ''
+
+    @api.depends('country_id', 'state_id', 'city_id')
+    def _change_real_city_province(self):
+        if self.country_id != self.env.ref('base.cl'):
+            self.real_city = False
+        if self.state_id == self.env.ref('base.state_cl_13'):
+            self.real_city = 'Santiago'
+        else:
+            self.real_city = self.city_id.name
+
+    @api.depends('country_id', 'state_id', 'city_id')
+    def _change_city_province(self):
+        if self.country_id != self.env.ref('base.cl'):
+            return
+        self.city = self.city_id.name
+
+    @api.depends('country_id', 'city_id')
+    def _change_state_province(self):
+        if self.country_id != self.env.ref('base.cl'):
+            return
+        self.state_id = self.city_id.state_id.parent_id
